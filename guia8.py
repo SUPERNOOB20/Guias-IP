d2460f8d29a8a6fa2623efec80578f5a44f139d4
SUPERNOOB20 <82103347+SUPERNOOB20@users.noreply.github.com>
2023-10-30 09:46:25 -0300

    Actualiza guia8.py

--- guia8.py
+++ guia8.py
@@ -222,3 +222,43 @@ def agruparPorLongitud(nombre_archivo: str) -> dict:
 
 agruparPorLongitud
 """
+
+
+
+# Ejercicio 7:
+from queue import Queue
+
+def a_clientes(clientes_cola: Queue) -> Queue:
+    cola_de_atencion: Queue = Queue()
+    lista_clientes: list = []
+    
+    while not clientes_cola.empty():
+        lista_clientes.append(clientes_cola.get())
+    print(lista_clientes)
+    for i in range(len(lista_clientes) - 1, -1, -1):
+        if lista_clientes[i][3] == True:
+            clientePrio = lista_clientes[i]
+            cola_de_atencion.put(clientePrio)
+            lista_clientes.remove(clientePrio)
+            print("ok")
+    for i in range(len(lista_clientes) - 1, -1, -1):
+        if lista_clientes[i][2] == True:
+            clientePref = lista_clientes[i]
+            cola_de_atencion.put(clientePref)
+            lista_clientes.remove(clientePref)
+            print("recontraok")
+    for i in range(len(lista_clientes) - 1, -1, -1):
+        cola_de_atencion.put(lista_clientes[i])
+        print("wat")
+    print(list(cola_de_atencion.queue))
+    return cola_de_atencion
+
+
+clientes_cola: Queue = Queue()
+clientes_cola.put(("cuarto", 1, True, False))
+clientes_cola.put(("quinto", 4, False, False))
+clientes_cola.put(("primero2", 2, True, True))
+clientes_cola.put(("primero1", 5,True, True))
+clientes_cola.put(("primero3", 4, False, True))
+
+print(a_clientes(clientes_cola))
