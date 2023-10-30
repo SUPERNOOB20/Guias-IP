50dd17dbeddcb54964d790dae9f23ef4fcd88b7e
SUPERNOOB20 <82103347+SUPERNOOB20@users.noreply.github.com>
2023-10-30 10:08:37 -0300

    Update guia8.py

--- guia8.py
+++ guia8.py
@@ -226,6 +226,7 @@ agruparPorLongitud
 
 
 # Ejercicio 7:
+"""
 from queue import Queue
 
 def a_clientes(clientes_cola: Queue) -> Queue:
@@ -248,6 +249,51 @@ def a_clientes(clientes_cola: Queue) -> Queue:
             lista_clientes.remove(clientePref)
             print("recontraok")
     for i in range(len(lista_clientes) - 1, -1, -1):
+        cola_de_atencion.put(lista_clientes[i])
+        print("wat")
+    print(list(cola_de_atencion.queue))
+    return cola_de_atencion
+
+
+clientes_cola: Queue = Queue()
+clientes_cola.put(("cuarto", 1, True, False))
+clientes_cola.put(("quinto", 4, False, False))
+clientes_cola.put(("primero2", 2, True, True))
+clientes_cola.put(("primero1", 5,True, True))
+clientes_cola.put(("primero3", 4, False, True))
+
+print(a_clientes(clientes_cola))
+"""
+
+
+from queue import Queue
+
+def a_clientes(clientes_cola: Queue) -> Queue:
+    cola_de_atencion: Queue = Queue()
+    lista_clientes: list = []
+    
+    while not clientes_cola.empty():
+        lista_clientes.append(clientes_cola.get())
+    # print(lista_clientes)
+    for i in range(0, len(lista_clientes)):
+        if lista_clientes[i][3] == True:
+            clientePrio = lista_clientes[i]
+            cola_de_atencion.put(clientePrio)
+            lista_clientes[i] = "BORRAR"
+            # lista_clientes.remove(clientePrio)
+            print("ok")
+    while "BORRAR" in lista_clientes:
+        lista_clientes.remove("BORRAR")
+    for i in range(0, len(lista_clientes)):
+        if lista_clientes[i][2] == True:
+            clientePref = lista_clientes[i]
+            cola_de_atencion.put(clientePref)
+            # lista_clientes.remove(clientePref)
+            lista_clientes[i] = "BORRAR"
+            print("recontraok")
+    while "BORRAR" in lista_clientes:
+        lista_clientes.remove("BORRAR")
+    for i in range(0, len(lista_clientes)):
         cola_de_atencion.put(lista_clientes[i])
         print("wat")
     print(list(cola_de_atencion.queue))
